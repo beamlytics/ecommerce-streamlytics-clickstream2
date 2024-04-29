246592ffd47e163fc30aa7a2bfa608b98a8b4693
Ritesh Tarway <rtarway@gmail.com>
2024-04-29 02:42:52 -0500

    modified POM.xml

--- src/main/java/com/beamlytics/inventory/businesslogic/core/options/RetailPipelineOptions.java
+++ src/main/java/com/beamlytics/inventory/businesslogic/core/options/RetailPipelineOptions.java
@@ -19,6 +19,7 @@ package com.beamlytics.inventory.businesslogic.core.options;
 
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.options.Default;
+//import org.apache.beam.sdk.options.Default;
 
 // @Experimental
 public interface RetailPipelineOptions
